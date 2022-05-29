2e8de2c91e9400052584cd968a675bf6f7e938f6
Priyansh Agrawal <agrawal.priyansh@yahoo.in>
2022-05-29 13:39:02 +0530

    Update 318

--- 318.py
+++ 318.py
@@ -24,3 +24,25 @@ class Solution:
                     mprod = max(mprod, len(words[i])*len(words[j]))
                     
         return mprod if mprod != float('-inf') else 0
+
+
+# Solution 2 (AC)
+# Use pure bit manipulation to generate binary representations of words.
+class Solution:
+    def getBin(self, word):
+        bit = 0
+        for char in word:
+            bit |= 1 << (ord(char) - ord('a'))
+            
+        return bit
+    
+    def maxProduct(self, words: List[str]) -> int:
+        bins = [self.getBin(w) for w in words]
+        
+        mprod = -inf
+        for i in range(len(words)):
+            for j in range(i+1, len(words)):
+                if bins[i] & bins[j] == 0:
+                    mprod = max(mprod, len(words[i])*len(words[j]))
+                    
+        return mprod if mprod != -inf else 0
