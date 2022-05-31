8c4e2709f91a2d333b1c44dbac256d6a67a1b618
Priyansh Agrawal <agrawal.priyansh@yahoo.in>
2022-05-31 12:39:57 +0530

    Update 1461

--- 1461.py
+++ 1461.py
@@ -2,6 +2,7 @@
 # 1461. Check If a String Contains All Binary Codes of Size K
 
 # Solution 1 (AC)
+# O(nk) sliding window 
 class Solution:
     def hasAllCodes(self, s: str, k: int) -> bool:
         isThere = [False]*(1<<k)
@@ -15,3 +16,15 @@ class Solution:
            
         #print(isThere)
         return all(x is True for x in isThere)
+
+# Solution 2 (AC)
+# O(n) Rolling hash - looked at Discuss
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        prev = int(s[:k], 2)
+        vals = {prev}
+        for i in range(k, len(s)):
+            prev = prev*2 - (int(s[i-k])*(1<<k)) + int(s[i])
+            vals.add(prev)
+            
+        return len(vals) == (1<<k)
