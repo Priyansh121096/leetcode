49a39a13aa476336341f4cd99085be3febd2ad4f
Priyansh Agrawal <agrawal.priyansh@yahoo.in>
2022-06-08 23:42:18 +0530

    Update 543

--- 543.py
+++ 543.py
@@ -9,22 +9,34 @@
 #         self.right = right
 class Solution:
     def dfs(self, node, height):
+        """
+        :returns: A 2-tuple (height, diameter) of the subtree rooted at `node`.
+        """
         if not node:
+            # Node is NULL; height and diameter are 0.
             return 0, 0
-        
+
         if not (node.left or node.right):
+            # Node has no children; height is 1; diameter is 0.
             return 1, 0
-        
+
+        # Get heights and diameters of left and right subtrees.
         lh, ld = self.dfs(node.left, height)
         rh, rd = self.dfs(node.right, height)
-        
+
+        # Height of the subtree rooted at `node` is 1 plus the
+        # maximum of the heights of left and right subtrees.
         height = max(lh, rh) + 1
+
+        # Diameter of the subtree rooted at `node` is the maximum of:
+        # A) The diameter of left subtree
+        # B) The diameter of right subtree
+        # C) The sum of the heights of left and right subtrees.
+        # If the max is C, it means that the diametric path goes through `node`.
         diameter = max(ld, rd, lh+rh)
-        
+
         return height, diameter
         
     
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         return self.dfs(root, 0)[1]
-        
-        
