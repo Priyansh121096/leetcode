b5e60510a190c1e5fb4f5dafe5d1ac36944331a7
Priyansh Agrawal <agrawal.priyansh@yahoo.in>
2022-06-08 22:06:12 +0530

    Add 1332

---
+++ 1332.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/remove-palindromic-subsequences/
+# 1332. Remove Palindromic Subsequences
+
+class Solution:
+    def removePalindromeSub(self, s: str) -> int:
+        """
+        If the string is empty; we can remove all palindromes in 0 steps.
+        If the string is a palindrome; we can remove all palindromes in 1 step.
+        Else, we can remove all 'a's once and all 'b's once to make the string
+        empty. This is because we need to remove subsequences (not substrings)
+        and strings containing all 'a's and all 'b's are palindromes.
+        """
+        N = len(s)
+        
+        if N % 2 == 0 and s[N//2-1] != s[N//2]:
+            return 2
+        
+        if N % 2 == 0:
+            left, right = N//2 - 2, N//2 + 1
+        else:
+            left, right = N//2 - 1, N//2 + 1
+            
+        try:
+            while s[left] == s[right]:
+                left -= 1
+                right += 1
+        except IndexError:
+            return 1
+
+        return 2
