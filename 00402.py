22cc28ddfd5d0d795a4e481e2f1abff4f7fc31c1
Priyansh Agrawal <agrawal.priyansh@yahoo.in>
2024-04-11 21:08:15 +0100

    Create 00402.py

---
+++ 00402.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/remove-k-digits
+
+# Solution #1 - Backtracking - Gives TLE
+
+def set_bit(value, bit):
+    return value | (1<<bit)
+
+def clear_bit(value, bit):
+    return value & ~(1<<bit)
+
+class Solution:
+    def minNumber(self, bits: int, k: int):
+        if k == self.maxK:
+            res, mul = 0, 1
+            for i in range(self.N-1, -1, -1):
+                if bits & 1 == 1:
+                    res += mul*self.num[i]
+                    mul *= 10
+                bits >>= 1
+
+            return res
+
+        minResult = float("inf")
+        for i in range(self.N):
+            new_bits = clear_bit(bits, i)
+            if new_bits == bits:
+                continue
+
+            minResult = min(minResult, self.minNumber(new_bits, k+1))
+
+        return minResult if minResult != float("inf") else 0
+
+    def removeKdigits(self, num: str, k: int) -> str:
+        self.num, self.N, self.maxK = [int(x) for x in num], len(num), k
+        return str(self.minNumber((2**self.N)-1, 0))
