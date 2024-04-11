9b929de4697ea0eaa87869344477511f63144f46
Priyansh Agrawal <agrawal.priyansh@yahoo.in>
2024-04-11 23:29:07 +0100

    Update 00402.py

--- 00402.py
+++ 00402.py
@@ -63,3 +63,77 @@ class Solution:
 
         new_num = self.removeOne(num[:len(num)-k+1])
         return self.removeKdigits(f"{new_num}{num[len(num)-k+1:]}", k-1)
+
+# Solution #3
+# Same as #1 but with bit manipulation
+import sys
+
+sys.set_int_max_str_digits(2*(10**5))
+
+def reset_last_n_bits(bits, n):
+    mask = (2**(10**5) - 1) << n
+    return bits & mask
+
+def set_last_n_bits(bits, n):
+    mask = (2**(n) - 1)
+    return bits | mask
+
+def reset_last_nth_bit(bits, n):
+    return bits & ~(1<<n)
+
+def p(bits: int):
+    print(f"{bits:b}")
+
+class Solution:
+    def removeOne(self, bits: int):
+        N = len(self.num)
+        minRes, minBits = float("inf"), None
+        
+        for i in range(N):
+            curr_bit = (bits >> (N-i-1)) & 1
+            if not curr_bit:
+                continue
+
+            res, mul = 0, 1
+            new_bits = reset_last_nth_bit(bits, N-i-1)
+            new_bits_copy = new_bits
+            for j in range(N-1, -1, -1):
+                if new_bits & 1 == 1:
+                    res += mul*self.num[j]
+                    mul *= 10
+
+                new_bits >>= 1
+
+            if res < minRes:
+                minRes, minBits = res, new_bits_copy
+
+        return minBits if minRes != float("inf") else 0
+
+    def removeKdigitsHelper(self, bits: int, k: int) -> str:
+        # print("rkdh start")
+        p(bits)
+        print(k)
+        if k == 1:
+            return self.removeOne(bits)
+
+        new_bits = self.removeOne(reset_last_n_bits(bits, k-1))
+        # print("rkdh mid")
+        p(new_bits)
+
+        return self.removeKdigitsHelper(set_last_n_bits(new_bits, k-1), k-1)
+
+    def removeKdigits(self, num: str, k: int) -> str:
+        N = len(num)
+        self.num = [int(x) for x in num]
+        
+        final_bits = self.removeKdigitsHelper(((2**N)-1), k)
+        p(final_bits)
+        
+        res, mul = 0, 1
+        for i in range(N-1, -1, -1):
+            if (final_bits & 1) == 1:
+                res += mul*self.num[i]
+                mul *= 10
+            final_bits >>= 1
+
+        return str(res)
