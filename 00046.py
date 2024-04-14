9d00e937c9c26772e60bae7d9e9077bb4719f29e
Priyansh Agrawal <agrawal.priyansh@yahoo.in>
2024-04-14 20:17:35 +0100

    Update 00046.py

--- 00046.py
+++ 00046.py
@@ -2,32 +2,19 @@
 # 46. Permutations
 
 class Solution:
-    def helper(self, rem, perm):
+    def helper(self, perm, rem):
         if not rem:
-            self.out.append(perm[:])
+            self.all_.append(perm[:])
             return
-        
-        for num in rem:
-            perm.append(num)
-            nrem = rem - {num}
-            self.helper(nrem, perm)
+
+        for x in rem:
+            perm.append(x)
+            rem.remove(x)
+            self.helper(perm, set(rem))
+            rem.add(x)
             perm.pop()
-    
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        self.nums = nums
-        self.out = []
-        
-        for num in nums:
-            rem = set(nums) - {num}
-            self.helper(rem, [num])
-            
-        return self.out
 
-
-class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return nums and [
-            p[:i] + [nums[0]] + p[i:]
-            for p in self.permute(nums[1:])
-            for i in range(len(nums))
-        ] or [[]]
+        self.all_ = []
+        self.helper([], set(nums))
+        return self.all_
