bbbd192574622e0ab282951e6644847742baf8a1
Priyansh Agrawal <agrawal.priyansh@yahoo.in>
2024-04-14 19:33:53 +0100

    Update 00236.py

--- 00236.py
+++ 00236.py
@@ -40,17 +40,32 @@ class Solution:
 #         self.left = None
 #         self.right = None
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # If it's a null node, return None
+        # If you've reached either of p and q, that node is the LCA
+        # between p and q in the subtree rooted at p or q.
         if root in (None, p, q):
             return root
         
+        # Find LCAs of p and q in the left and the right subtrees.
         left, right = self.lowestCommonAncestor(root.left, p, q), self.lowestCommonAncestor(root.right, p, q)
         
+        # If neither of them exist, it means we weren't able to p and q in any subtree.
         if not left and not right:
             return 
         
+        # If both exist, it means we found p in one subtree and q in the other. So, LCA is root.
         if left and right:
             return root
         
+        # If only one of them exists, it means we found both p and q in either the left or the right
+        # subtree. This means the LCA is either the left or the right (whichever is non-null.)
         return left or right
