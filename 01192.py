c5648927ec308e3b8f3dfc24db2256c28a7cc850
Priyansh Agrawal <agrawal.priyansh@yahoo.in>
2025-07-14 21:16:18 +0100

    Update 01192.py

--- 01192.py
+++ 01192.py
@@ -27,3 +27,42 @@ class Solution:
                     bridges.append([u, v])
             else:
                 low[u] = min(low[u], disc[v])
+
+
+class Solution:
+    def criticalConnections(self, n: int, edges: list[list[int]]) -> list[list[int]]:
+        graph = defaultdict(list)
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+
+        edges = set(map(tuple, (map(sorted, edges))))
+        rank = [-2] * n
+
+        def dfs(node, depth):
+            if rank[node] >= 0:
+                # visiting (0<=rank<n), or visited (rank=n)
+                return rank[node]
+
+            rank[node] = depth
+
+            min_back_depth = n
+            for neighbor in graph[node]:
+                if rank[neighbor] == depth - 1:
+                    continue  # don't immmediately go back to parent. that's why i didn't choose -1 as the special value, in case depth==0.
+
+                # Find the minimum depth a neighbor can reach
+                back_depth = dfs(neighbor, depth + 1)
+
+                # If a neighbor can lead back to an ancestor of current node
+                # (or the node itself not via the node-neighbor edge),
+                # then the node-neighbor is not critical
+                if back_depth <= depth:
+                    edges.discard(tuple(sorted((node, neighbor))))
+
+                min_back_depth = min(min_back_depth, back_depth)
+
+            return min_back_depth
+
+        dfs(0, 0)  # since this is a connected graph, we don't have to loop over all nodes.
+        return list(edges)
