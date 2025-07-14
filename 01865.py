bea3dc0409a776e13b5c06b85778ed80413964d2
Priyansh Agrawal <agrawal.priyansh@yahoo.in>
2025-07-14 01:14:15 +0100

    Update 01865.py

--- 01865.py
+++ 01865.py
@@ -3,6 +3,38 @@
 from collections import Counter
 import bisect
 
+from collections import Counter
+
+class FindSumPairs:
+
+    def __init__(self, nums1: List[int], nums2: List[int]):
+        self.nums1 = nums1
+        self.c1 = Counter(nums1)
+        
+        self.nums2 = nums2
+        self.c2 = Counter(nums2)
+
+    def add(self, index: int, val: int) -> None:
+        old_val = self.nums2[index]
+        new_val = old_val + val
+        self.nums2[index] = new_val
+
+        self.c2[old_val] -= 1
+        self.c2[new_val] += 1
+
+    def count(self, tot: int) -> int:
+        count = 0
+        for val in self.c1:
+            look_for = tot - val
+            count += self.c1[val] * self.c2[look_for]
+
+        return count
+
+# Your FindSumPairs object will be instantiated and called as such:
+# obj = FindSumPairs(nums1, nums2)
+# obj.add(index,val)
+# param_2 = obj.count(tot)
+
 class FindSumPairs:
 
     def __init__(self, nums1: List[int], nums2: List[int]):
